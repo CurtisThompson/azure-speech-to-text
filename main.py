cfdaffbc98df0ef677ca2998c9ac39fb5901d232
Curtis Thompson <curtiswaynethompson@gmail.com>
2023-08-22 16:07:44 +0800

    Read start of mp3 as per quickstart

--- main.py
+++ main.py
@@ -1,11 +1,69 @@
 import yaml
 
+import azure.cognitiveservices.speech as speechsdk
+
 with open('config.yml', 'r') as f:
     config = yaml.safe_load(f)
 
-print(config['azure']['key'])
-print(config['azure']['endpoint'])
-print(config['azure']['wait_ms'])
-print(config['data']['input_path'])
-print(config['data']['output_path'])
-print(config['verbose'])
+AZ_KEY = config['azure']['key']
+AZ_REGION = config['azure']['region']
+
+INPUT_FILE = config['data']['input_path']
+
+def speech_recognize_once_compressed_input():
+    """performs one-shot speech recognition with compressed input from an audio file"""
+    class BinaryFileReaderCallback(speechsdk.audio.PullAudioInputStreamCallback):
+        def __init__(self, filename: str):
+            super().__init__()
+            self._file_h = open(filename, "rb")
+
+        def read(self, buffer: memoryview) -> int:
+            try:
+                size = buffer.nbytes
+                frames = self._file_h.read(size)
+
+                buffer[:len(frames)] = frames
+
+                return len(frames)
+            except Exception as ex:
+                print('Exception in `read`: {}'.format(ex))
+                raise
+
+        def close(self) -> None:
+            print('closing file')
+            try:
+                self._file_h.close()
+            except Exception as ex:
+                print('Exception in `close`: {}'.format(ex))
+                raise
+    # Creates an audio stream format. For an example we are using MP3 compressed file here
+    compressed_format = speechsdk.audio.AudioStreamFormat(compressed_stream_format=speechsdk.AudioStreamContainerFormat.MP3)
+    callback = BinaryFileReaderCallback(filename=INPUT_FILE)
+    stream = speechsdk.audio.PullAudioInputStream(stream_format=compressed_format, pull_stream_callback=callback)
+
+    speech_config = speechsdk.SpeechConfig(subscription=AZ_KEY, region=AZ_REGION)
+    audio_config = speechsdk.audio.AudioConfig(stream=stream)
+
+    # Creates a speech recognizer using a file as audio input, also specify the speech language
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config, audio_config)
+
+    # Starts speech recognition, and returns after a single utterance is recognized. The end of a
+    # single utterance is determined by listening for silence at the end or until a maximum of 15
+    # seconds of audio is processed. It returns the recognition text as result.
+    # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
+    # shot recognition like command or query.
+    # For long-running multi-utterance recognition, use start_continuous_recognition() instead.
+    result = speech_recognizer.recognize_once()
+
+    # Check the result
+    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("Recognized: {}".format(result.text))
+    elif result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(result.no_match_details))
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+
+speech_recognize_once_compressed_input()
