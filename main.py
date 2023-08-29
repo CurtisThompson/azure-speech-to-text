385f5eb7637e69f5ebc19207017040965036995e
Curtis Thompson <curtiswaynethompson@gmail.com>
2023-08-29 16:16:36 +0700

    Clean WAV speech to text code

--- main.py
+++ main.py
@@ -11,97 +11,12 @@ AZ_REGION = config['azure']['region']
 
 INPUT_FILE = config['data']['input_path']
 
-def speech_recognize_once_compressed_input():
-    """performs one-shot speech recognition with compressed input from an audio file"""
-    class BinaryFileReaderCallback(speechsdk.audio.PullAudioInputStreamCallback):
-        def __init__(self, filename: str):
-            super().__init__()
-            self._file_h = open(filename, "rb")
-
-        def read(self, buffer: memoryview) -> int:
-            try:
-                size = buffer.nbytes
-                frames = self._file_h.read(size)
-
-                buffer[:len(frames)] = frames
-
-                return len(frames)
-            except Exception as ex:
-                print('Exception in `read`: {}'.format(ex))
-                raise
-
-        def close(self) -> None:
-            print('closing file')
-            try:
-                self._file_h.close()
-            except Exception as ex:
-                print('Exception in `close`: {}'.format(ex))
-                raise
-    # Creates an audio stream format. For an example we are using MP3 compressed file here
-    compressed_format = speechsdk.audio.AudioStreamFormat(compressed_stream_format=speechsdk.AudioStreamContainerFormat.MP3)
-    callback = BinaryFileReaderCallback(filename=INPUT_FILE)
-    stream = speechsdk.audio.PullAudioInputStream(stream_format=compressed_format, pull_stream_callback=callback)
-
-    speech_config = speechsdk.SpeechConfig(subscription=AZ_KEY, region=AZ_REGION)
-    audio_config = speechsdk.audio.AudioConfig(stream=stream)
-
-    # Creates a speech recognizer using a file as audio input, also specify the speech language
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config, audio_config)
-
-    # Starts speech recognition, and returns after a single utterance is recognized. The end of a
-    # single utterance is determined by listening for silence at the end or until a maximum of 15
-    # seconds of audio is processed. It returns the recognition text as result.
-    # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
-    # shot recognition like command or query.
-    # For long-running multi-utterance recognition, use start_continuous_recognition() instead.
-    result = speech_recognizer.recognize_once()
-
-    # Check the result
-    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print("Recognized: {}".format(result.text))
-    elif result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(result.no_match_details))
-    elif result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
-
 
 def speech_recognize_continuous_from_file():
     """performs continuous speech recognition with input from an audio file"""
-    class BinaryFileReaderCallback(speechsdk.audio.PullAudioInputStreamCallback):
-        def __init__(self, filename: str):
-            super().__init__()
-            self._file_h = open(filename, "rb")
-
-        def read(self, buffer: memoryview) -> int:
-            try:
-                size = buffer.nbytes
-                frames = self._file_h.read(size)
-
-                buffer[:len(frames)] = frames
-
-                return len(frames)
-            except Exception as ex:
-                print('Exception in `read`: {}'.format(ex))
-                raise
-
-        def close(self) -> None:
-            print('closing file')
-            try:
-                self._file_h.close()
-            except Exception as ex:
-                print('Exception in `close`: {}'.format(ex))
-                raise
-    # Creates an audio stream format from an MP3 file
-    #compressed_format = speechsdk.audio.AudioStreamFormat(compressed_stream_format=speechsdk.AudioStreamContainerFormat.MP3)
-    #callback = BinaryFileReaderCallback(filename=INPUT_FILE)
-    #stream = speechsdk.audio.PullAudioInputStream(stream_format=compressed_format, pull_stream_callback=callback)
 
     # Configure Azure speech to text based on stream and API key
     speech_config = speechsdk.SpeechConfig(subscription=AZ_KEY, region=AZ_REGION)
-    #audio_config = speechsdk.audio.AudioConfig(stream=stream)
     audio_config = speechsdk.audio.AudioConfig(filename=INPUT_FILE)
 
     # Set up speech to text connection
@@ -114,25 +29,18 @@ def speech_recognize_continuous_from_file():
     # Stopping callback
     def stop_cb(evt):
         """callback that stops continuous recognition upon receiving an event `evt`"""
-        #print('CLOSING on {}'.format(evt))
         speech_recognizer.stop_continuous_recognition()
         nonlocal done
-        #final_result = evt.result.text
         done = True
     
     def recognised_cb(evt):
         """Callback that saves text when API has recognised solution."""
         nonlocal final_result
-        #print(evt.result.text)
         final_result += evt.result.text + ' '
-        #print(final_result)
 
     # Connect callbacks to the events fired by the speech recognizer
-    #speech_recognizer.recognizing.connect(lambda evt: print(evt.result.text))
+    # Unused events are recognizing, session_started, session_stopped, canceled
     speech_recognizer.recognized.connect(recognised_cb)
-    #speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
-    #speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
-    #speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
     # Stop continuous recognition on either session stopped or canceled events
     speech_recognizer.session_stopped.connect(stop_cb)
     speech_recognizer.canceled.connect(stop_cb)
@@ -142,10 +50,9 @@ def speech_recognize_continuous_from_file():
     while not done:
         time.sleep(.5)
     
+    # Return final result once whole file converted to text
     if done:
-        print(final_result)
         return final_result
 
 
-#speech_recognize_once_compressed_input()
 speech_recognize_continuous_from_file()
